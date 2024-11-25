b55670ab8be0aa846894a57d5a7766e5f32ece94
theAdmin10 <ronald.d.waller@gmail.com>
2024-11-25 13:24:53 -0600

    Added some functions for get_book_text(), word_count(), and get_letters()

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-with open("/home/ronald/src/boot.dev/04-Build-a-Bookbot/bookbot/books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    #book = "books/frankenstein.txt"
+    book = "/home/ronald/src/boot.dev/04-Build-a-Bookbot/bookbot/books/frankenstein.txt"
+    text = get_book_text(book)
+    words = word_count(text)
+    letters = get_letters(text)
+    print(f"{words} words found in the book")
+    print(f"{letters}")
+    return 0
 
-words = file_contents.split()
-print(len(words))
+def get_book_text(b):
+    with open(b) as f:
+        return f.read()
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+def get_letters(text):
+    letters = {}
+    words = text.split()
+    for word in words:
+        word = word.lower()
+        for letter in word:
+            if (letter not in letters):
+                letters[letter] = 1
+            else:
+                letters[letter] += 1
+    return letters
+
+main()
